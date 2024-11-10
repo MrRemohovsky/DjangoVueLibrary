103fd44be315d0346d15f673ca668b06136f997f
Remoh <arshanov8807@mail.ru>
2024-11-10 17:16:38 +0300

    feat(admin): настройка отображения моделей в admin панели django

--- backend/locallibrary/catalog/admin.py
+++ backend/locallibrary/catalog/admin.py
@@ -2,7 +2,33 @@ from django.contrib import admin
 from .models import Genre, Author, Language, Book, BookInstance
 
 admin.site.register(Genre)
-admin.site.register(Author)
 admin.site.register(Language)
-admin.site.register(Book)
-admin.site.register(BookInstance)
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+class BookInline(admin.TabularInline):
+    model = Book.author.through
+    extra = 0
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_author', 'display_genre', 'language')
+    inlines = [BookInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'borrower')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {'fields': ('book', 'id', 'borrower')}),
+        ('Availability', {'fields': ('status', 'due_back')}),
+    )
+
