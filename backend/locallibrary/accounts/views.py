7c0ac958a25307292fc79e3a5f18dbb84b8058cb
Remoh <arshanov8807@mail.ru>
2024-11-25 14:32:03 +0300

    feat(): создание api для авторизации, обновления токена и выхода из аккаунта

--- backend/locallibrary/accounts/views.py
+++ backend/locallibrary/accounts/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from .serializers import RegisterSerializer
 
 class RegisterView(APIView):
@@ -12,3 +15,15 @@ class RegisterView(APIView):
             serializer.save()
             return Response({'message': 'Пользователь был успешно создан!'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        try:
+            refresh_token = request.data.get('refresh')
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"detail": "Successfully logged out"}, status = status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"detail": "Invalid token or already blacklisted"}, status.HTTP_400_BAD_REQUEST)
+
